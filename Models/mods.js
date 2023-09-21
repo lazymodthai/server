5e029bae2102a014e45634f95b6108259cce7e00
buffaro <buffaro_master@hotmail.com>
2023-09-21 07:22:23 +0700

    Fix: modsSchema

--- Models/mods.js
+++ Models/mods.js
@@ -6,16 +6,12 @@ const modsSchema = mongoose.Schema(
     mod_name: { type: String, required: true },
     mod_version: { type: String, default: "1.0.0" },
     game_name: { type: String, required: true },
-    game_link: {
-      steam: { type: String, default: null },
-      epic: { type: String, default: null },
-      xbox: { type: String, default: null },
-      gog: { type: String, default: null },
-      ea: { type: String, default: null },
-      ubisoft: { type: String, default: null },
-      battlenet: { type: String, default: null },
-      other: { type: String, default: null },
-    },
+    game_link: [
+      {
+        platform: { type: String, default: null },
+        link: { type: String, default: null },
+      },
+    ],
     mod_status: { type: Number, default: 0 },
     download_link: [
       {
