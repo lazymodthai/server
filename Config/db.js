37a9bf202df8cffba0105a74335d98aaa25623d3
buffaro <buffaro_master@hotmail.com>
2023-09-26 09:45:30 +0700

    Fix: auth

--- Config/db.js
+++ Config/db.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose')
-const DBLINK = require('dotenv').config()
-const url = process.env.DBLINK
+const mongoose = require("mongoose");
+const DBLINK = require("dotenv").config();
+const url = process.env.DBLINK;
 
-const connectDB = async()=>{
-    try{
-        await mongoose
-        .connect(url,{dbName: 'modthai'})
-        console.log('MongoDB connected');
-    }catch (err){
-        console.log(err);
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(url, { dbName: "modthai" });
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.log(err);
+    console.log("Cannot connect MongoDB");
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
--- Services/auth.service.js
+++ Services/auth.service.js
@@ -1,14 +1,19 @@
-
-const { CreateMember } = require("../repositories/member.repository");
-async function Register(body){
-    try {
-        console.log(req.body);
-        const addMember = CreateMember(body)
-        return addMember
-      } catch (err) {
-        console.log(err);
-        return err
-      }
+const { CreateMember } = require("../repositories/members.repository");
+const Members = require("../models/members.model");
+async function Register(body) {
+  try {
+    console.log(body);
+    const { username, password } = body;
+    const user = await Members.findOne({ username });
+    if (user) {
+      console.log('This username has already exist');
+    } else {
+      CreateMember(body);
+      console.log('Create',body);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 module.exports = { Register };
