7e6d714fdc52a37324e199697f6756ff164bb419
kormpat <kormpat@focalsolution.com>
2023-09-27 21:18:58 +0700

    Revert "feat : learning js"
    This reverts commit 1cfd402d1a18c2c8bc3fc0ee3487b9600253a848.

--- Services/auth.service.js
+++ Services/auth.service.js
@@ -1,41 +1,20 @@
 const { CreateMember } = require("../repositories/members.repository");
 const Members = require("../models/members.model");
-
-
-function Test(){
-  let res = {data:"ok",status:200}
-  return res
+async function Register(body) {
+  try {
+    const { username, password } = body;
+    const user = await Members.findOne({ username });
+    // Check username already exist
+    if (user) {
+      console.log("This username has already exist")
+    } else {
+      // Create member
+      await CreateMember(body);
+      console.log("Create",body);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-async function TestAsync(){
-   number = await getNumber()
-   return number /2 
-}
-
-async function getNumber(){
-  data = await getPermission
-  // pause 10 sec
-  return 0
-}
-function fetchDataPromise(){
-  let data = {}
-  console.log("")
-  fetch("url").then(res => res.json).then(data = res)
-}
-
-async function fetchData (){
-  let data = await fetch("url")
-  data = data.json
- 
-}
-
-async function axiosV1(){
-  let {data} = await $axios.get(``)
-  let res = await $axios.post(``,body)
-}
-
-
-
-
-
-module.exports = {  axiosV2};
+module.exports = { Register };
--- server.js
+++ server.js
@@ -15,6 +15,6 @@ const { readdirSync } = require("fs");
 
 readdirSync("./Routes").map((r) => app.use("/api", require("./Routes/" + r)));
 
-app.listen(5001, () =>
+app.listen(5000, () =>
   console.log("Server is running on http://localhost:5000")
 );
